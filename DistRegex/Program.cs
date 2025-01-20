873dc2050bff0ca312209dccda4beb5d06da3e85
Ants <code.herder@recombulator.net>
2025-01-20 15:41:42 -0500

    Initial port

--- DistRegex/Program.cs
+++ DistRegex/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // DistRegex.Literal.DistRx.Main(["test.png", "8", "2*3?(4+1)+"]);
+            DistRegex.Literal.DistRx.Main(["test.png", "8", ".?2*1.3"]);
         }
     }
 }
